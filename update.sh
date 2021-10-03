056e5b3f470b53a6c760fc3581bf3c63c8f79934
Albresky <78648318+Albresky@users.noreply.github.com>
2021-10-03 20:08:05 +0800

    update master-branch 2021年10月 3日 20:08:05

--- update.sh
+++ update.sh
@@ -14,15 +14,15 @@ rm -rf public
 echo -e "\n hugo重构 \n" 
 hugo
 
-#备份.gitignore -> .gitignore.bak
-#echo -e "\n 备份.gitignore -> .gitignore.bak \n"
-#mv .gitignore .gitignore.bak
-
 git add .
 
 echo -e "\n 部署master分支 \n"
 git commit -m "update master-branch `date`"
 git push -f origin master
+
+#备份.gitignore -> .gitignore.bak
+echo -e "\n 备份.gitignore -> .gitignore.bak \n"
+mv .gitignore .gitignore.bak
 
 #部署gh-pages分支
 echo -e "\n 部署gh-pages分支 \n"
@@ -30,7 +30,7 @@ git add public && git commit -m "update 'gh-pages' `date`"
 git subtree push --prefix public origin gh-pages
 
 #恢复命名.gitignore
-#echo -e "\n 恢复.gitignore.bak -> .gitignore \n"
-#mv .gitignore.bak .gitignore
+echo -e "\n 恢复.gitignore.bak -> .gitignore \n"
+mv .gitignore.bak .gitignore
 
 read -n 1 -p "Press any key to continue..."
